a2d5797c3e8a3231f424595a49336a9e7afbc6d9
Leonardo David R.M <leodar1234@gmail.com>
2023-05-04 11:53:13 +0200

    prueba1

---
+++ index.js
@@ -0,0 +1,30 @@
+const suma = (x,y) =>{
+    resultado = x+y;
+    // console.log(resultado);
+    return restultado;
+};
+
+const resta = (x,y) => {
+    resultado = x-y;
+    // console.log(resultado);
+    return resultado;
+}
+
+const producto= (x,y) =>{
+    resultado = x*y;
+    // console.log(resultado);
+    return resultado;
+}
+
+const divison = (x,y) => {
+    resultado = x/y;
+    // console.log(resultado);
+    return resultado;
+}
+
+const cuadrado = (x) => {
+    resultado = x*x;
+    return resultado;
+}
+
+module.exports = {suma , resta , divison , producto};
